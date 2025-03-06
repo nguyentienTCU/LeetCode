b1fa9940e6bd72be73a9a5694b8a73b6cf421f7f
nguyentienTCU <nguyentien3036@gmail.com>
2025-03-06 00:18:36 -0600

    Time: 1 ms (98.42%), Space: 44.8 MB (43.13%) - LeetHub

---
+++ 1168-duplicate-zeros/1168-duplicate-zeros.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int n = arr.length;
+        int zeroCount = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i]==0){
+                zeroCount++;
+            }
+        }
+        for(int i = n-1; i>=0; i--){
+            if(i+zeroCount<n){
+                arr[i+zeroCount]=arr[i];
+            }
+            if(arr[i]==0){
+                zeroCount--;
+                if(i+zeroCount<n){
+                    arr[i+zeroCount]=0;
+                }
+            }
+        }
+    }
+}
