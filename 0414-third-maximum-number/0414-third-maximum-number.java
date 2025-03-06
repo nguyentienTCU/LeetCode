02c3f7ddb1676555a831446378d782e94056056c
nguyentienTCU <nguyentien3036@gmail.com>
2025-03-06 00:14:27 -0600

    Time: 0 ms (100%), Space: 43 MB (72.12%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        long max1 = Long.MIN_VALUE;
+        long max2 = Long.MIN_VALUE;
+        long max3 = Long.MIN_VALUE;
+
+        for(int i:nums){
+            if(i>max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = i;
+            }else if(i>max2 && i<max1){
+                max3 = max2;
+                max2 = i;
+            }else if(i>max3 && i<max2){
+                max3=i;
+            }
+        }
+        return max3==Long.MIN_VALUE ? (int) max1: (int) max3;
+    }
+}
