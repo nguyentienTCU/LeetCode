59370acc19237247964ab16a02dcf63e9d01bf40
Tien Nguyen <nguyentien3036@gmail.com>
2025-10-11 22:12:25 -0500

    Time: 141 ms (49.23%), Space: 60.3 MB (55.64%) - LeetHub

--- 1985-maximum-subarray-min-product/1985-maximum-subarray-min-product.java
+++ 1985-maximum-subarray-min-product/1985-maximum-subarray-min-product.java
@@ -3,9 +3,11 @@ class Solution {
         int n = nums.length;
         long MODULO = 1_000_000_007L;
         
+        //construct the prefix sum
         long[] prefixSum = new long[n+1];
         for(int i=0; i<n; i++) prefixSum[i+1]=prefixSum[i]+nums[i];
 
+        //setup for monotonic increasing/decreasing stacks
         int[] left = new int[n];
         int[] right = new int[n];
         Arrays.fill(left, -1);
@@ -13,6 +15,8 @@ class Solution {
 
         Stack<Integer> st = new Stack<>();
 
+        //strict increasing monotonic stack (pops equal sign)
+        //find the nearest index that has smaller value on the left: (2,3,3,1,2) -> (-1,0,0,-1,3)
         for(int i=0; i<n; i++){
             while(!st.isEmpty() && nums[st.peek()] >= nums[i]) st.pop();
             left[i] = st.isEmpty() ? -1 : st.peek();
@@ -21,6 +25,8 @@ class Solution {
 
         st.clear();
 
+        //non-strict increasing monotonic stack (does not pop equal sign)
+        //find the nearest index that has smaller value on the right: (2,3,3,1,2) -> (3,2,3,5,5)
         for(int i=n-1; i>=0; i--){
             while(!st.isEmpty() && nums[st.peek()] > nums[i]) st.pop();
             right[i] = st.isEmpty() ? n : st.peek();
@@ -29,6 +35,7 @@ class Solution {
 
         long max=0;
 
+        //for each element nums[i], find the largest range where nums[i] is the smallest value
         for(int i=0; i<n; i++){
             int l = left[i]+1;
             int r = right[i]-1;
