d21345050acafd354fe75cea2b0f5628b1c8c4ab
Tien Nguyen <nguyentien3036@gmail.com>
2025-10-22 22:43:31 -0500

    Time: 24 ms (25.06%), Space: 47.2 MB (17.63%) - LeetHub

--- 0475-heaters/0475-heaters.java
+++ 0475-heaters/0475-heaters.java
@@ -7,17 +7,23 @@ class Solution {
         for(int house : houses){
             int idx = lowerBound(house, heaters);   //index of the first element >= house in the array heaters
             long best = Long.MAX_VALUE;
+
+            //find the shortest distance from the current house to a heater
             if(idx<heaters.length){
-                best = Math.min(best, Math.abs((long)heaters[idx]-house));
+                best = Math.min(best, Math.abs((long)heaters[idx]-house));  //right heater
             }
             if(idx>=1){
-                best = Math.min(best, Math.abs((long)house-heaters[idx-1]));
+                best = Math.min(best, Math.abs((long)house-heaters[idx-1]));    //left heater
             }
+
+            //find the needed radius (longest distance)
             ans = Math.max(ans, best);
         }
         return (int) ans;
     }
 
+
+    //lowerBound: this method returns the leftmost index of the element in the array nums that is larger than target
     private int lowerBound(int target, int[] nums){
         int left = 0;
         int right = nums.length-1;
