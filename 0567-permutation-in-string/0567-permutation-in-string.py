513b7ed2d94527a478a1dada16134ac461b5a86b
Tien Nguyen <nguyentien3036@gmail.com>
2026-06-02 22:42:50 +0700

    Time: 9 ms (98.33%), Space: 19.4 MB (19.21%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        s1Array = [0] * 26
+        windowArray = [0] * 26
+        for i in range(len(s1)):
+            s1Array[ord(s1[i])-ord('a')] += 1
+            windowArray[ord(s2[i])-ord('a')] += 1
+        
+        if s1Array == windowArray:
+            return True
+
+        left = 0
+        for right in range(len(s1),len(s2)):
+            windowArray[ord(s2[right])-ord('a')] += 1
+            windowArray[ord(s2[left])-ord('a')] -= 1
+            left += 1
+
+            if s1Array == windowArray:
+                return True
+        return False
