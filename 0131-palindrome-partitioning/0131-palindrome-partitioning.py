32f25ed705d1575ac5f75ed8af02bd54f81c6111
Tien Nguyen <nguyentien3036@gmail.com>
2026-06-09 20:53:18 +0700

    Time: 34 ms (86.19%), Space: 34.4 MB (22.72%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,24 +1,23 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
+        n = len(s)
+        dp = [[False]*n for _ in range(n)]
+        for i in range(n-1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j] and (j-i <= 2 or dp[i+1][j-1]):
+                    dp[i][j] = True
+
         def backtrack(i, cur):
             if i >= len(s):
                 res.append(cur.copy())
                 return
             
             for j in range(i,len(s)):
-                if isPalindrome(s, i, j):
+                if dp[i][j]:
                     cur.append(s[i : j+1])
                     backtrack(j+1, cur)
                     cur.pop()
-
-        def isPalindrome(s: str, i: int, j: int):
-            while i < j:
-                if s[i] != s[j]:
-                    return False
-                i += 1
-                j -= 1
-            return True
 
         backtrack(0, [])
         return res
