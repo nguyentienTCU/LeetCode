e23fcc5cddac8f64abb04989ab950441932fbae7
Tien Nguyen <nguyentien3036@gmail.com>
2026-06-12 19:20:24 +0700

    Time: 0 ms (100%), Space: 19.3 MB (48.17%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # reach the current stair from 1 or 2 steps below
+        # dp[] stores values where dp[i] means the minumum cost to reach that stair
+        # dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
+        # dp[n] will be the top of the ladder
+
+        n = len(cost)
+        if n == 2:
+            return min(cost[0], cost[1])
+
+        firstPrev = 0
+        secondPrev = 0
+
+        for i in range(2, n+1):
+            cur = min(firstPrev + cost[i-1], secondPrev + cost[i-2])
+            secondPrev = firstPrev
+            firstPrev = cur
+
+        return cur
