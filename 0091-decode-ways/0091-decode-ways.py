4c902b85c439f1af7e3be2825ad1eab0d2fa5ca7
Tien Nguyen <nguyentien3036@gmail.com>
2026-06-14 11:05:05 +0700

    Time: 0 ms (100%), Space: 19.3 MB (40.79%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,13 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        dp = [0] * (n+1)
-        dp[n] = 1
+        dp = 0
+        dp1 = 1
+        dp2 = 0
         for i in range(n-1,-1,-1):
             if s[i] == "0":
-                dp[i] = 0
+                dp = 0
             elif (i+1 < n) and (s[i] == "1" or (s[i] == "2" and s[i+1] in ["0","1","2","3","4","5","6"])):
-                dp[i] = dp[i+1] + dp[i+2]
+                dp = dp1 + dp2
             else:
-                dp[i] = dp[i+1]
-        return dp[0]
+                dp = dp1
+            dp2 = dp1
+            dp1 = dp
+        return dp
